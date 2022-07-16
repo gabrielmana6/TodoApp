0ad2857e3288d45831e81177e09dc0dd01989792
Gabriel Manassés <gabrielmanasses13@gmail.com>
2022-07-16 04:17:30 -0300

    Criacao dos Controladores

---
+++ TodoApp/app/src/main/java/main/Main.java
@@ -0,0 +1,71 @@
+package main;
+
+import controller.ProjectController;
+import controller.TaskController;
+import java.util.Date;
+import java.util.List;
+import model.Project;
+import model.Task;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+//        ProjectController projectController = new ProjectController();
+//        
+//        Project project = new Project();
+//        
+//        project.setName("Projeto teste");
+//        project.setDescription("descricao");
+//        projectController.save(project);
+
+
+//        ProjectController projectController = new ProjectController();
+//        
+//        Project project = new Project();
+//        project.setId(1);
+//        project.setName("Novo nome do projeto 1");
+//        project.setDescription("descricaozada");
+//        
+//        projectController.update(project);
+//        
+//        List<Project> projects = projectController.getAll();
+//        System.out.println("Total de projetos = " + projects.size());
+//        
+//        projectController.removeById(1);
+
+        
+        
+//        ProjectController projectController = new ProjectController();
+//        Project project = new Project();
+//        
+//        project.setName("Meu projeto");
+//        project.setDescription("Descricao do projeto");
+//        
+//        projectController.save(project);
+//        
+//        List<Project> projects = projectController.getAll();
+//        System.out.println("Total de projetos = " + projects.size());
+        
+        
+//        TaskController taskController = new TaskController();
+//        Task task = new Task();
+//        
+//        task.setIdProject(2);
+//        task.setName("Minha tarefa");
+//        task.setDescription("Descricao da tarefa");
+//        task.setIsCompleted(false);
+//        task.setDeadline(new Date());
+//        
+//        taskController.save(task);
+//        
+//        List<Task> tasks = taskController.getAll(2);
+//        System.out.println("Total de tarefas = " + tasks.size());
+        
+//        taskController.removeById();
+        
+        
+        
+        
+    }
+}
