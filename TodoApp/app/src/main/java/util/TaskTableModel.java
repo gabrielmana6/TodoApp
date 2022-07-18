b1dda283053be0aa6e4b6867242385e37da9b8c4
Gabriel Manassés <gabrielmanasses13@gmail.com>
2022-07-18 01:58:19 -0300

    16 - Implementando o Model da Lista de Tarefas

--- TodoApp/app/src/main/java/util/TaskTableModel.java
+++ TodoApp/app/src/main/java/util/TaskTableModel.java
@@ -59,6 +59,11 @@ public class TaskTableModel extends AbstractTableModel{
                 return "Dados não encontrados";
         }
     }
+    
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex){
+        tasks.get(rowIndex).setIsCompleted((boolean) aValue);
+    }
 
     public String[] getColumns() {
         return columns;
